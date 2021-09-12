43766e55a86a4cf40d17a9c2585929104f3c2e8a
sylwia-roma <44410474+sylwia-roma@users.noreply.github.com>
2021-09-12 17:39:56 +0200

    Add files via upload

---
+++ Reverse words(7).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("de ef rge"));
+        }
+
+        public static string ReverseWords(string original)
+        {
+           string result = string.Join(" ", original.Split(' ').Select(x => new string(x.Reverse().ToArray())));
+            return result;
+        }
+    }
+}
