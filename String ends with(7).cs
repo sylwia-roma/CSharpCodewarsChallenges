8fb6d2db54b3ecfcbac45659cd36190b853077bc
sylwia-roma <44410474+sylwia-roma@users.noreply.github.com>
2021-09-12 16:54:37 +0200

    Add files via upload

---
+++ String ends with(7).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodewarsChallenges
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution("sumo", "omokok"));
+        }
+
+        public static bool Solution(string str, string ending)
+        {
+            return str.EndsWith(ending);
+        }
+    }
+}
