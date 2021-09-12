c5dc10df5056eb3a0841ce5cfa6db2b8c0b15d90
sylwia-roma <44410474+sylwia-roma@users.noreply.github.com>
2021-09-12 17:58:25 +0200

    Update Ch4113ng3(7).cs

--- Ch4113ng3(7).cs
+++ Ch4113ng3(7).cs
@@ -1,3 +1,5 @@
+//Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
