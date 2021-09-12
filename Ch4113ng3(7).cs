432b8383016f14e9f9203c3f1a6938f47a622fd3
sylwia-roma <44410474+sylwia-roma@users.noreply.github.com>
2021-09-12 17:57:47 +0200

    Add files via upload

---
+++ Ch4113ng3(7).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Ch4113ng3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Nerdify("Los Angeles"));
+        }
+
+        public static string Nerdify(string str)
+        {
+            string newString = str.Replace("A", "4").Replace("a", "4").Replace("E", "3").Replace("e", "3").Replace("l", "1");
+            return newString;
+        }
+    }
+}
